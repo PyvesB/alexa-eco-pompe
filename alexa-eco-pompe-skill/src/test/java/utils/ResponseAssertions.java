6ae4ced70418d4972a19912b935bf0a302455f48
PyvesB <PyvesDev@gmail.com>
2018-06-20 10:11:21 +0200

    Covered null case in assertions for clearer test failure messages

--- alexa-eco-pompe-skill/src/test/java/utils/ResponseAssertions.java
+++ alexa-eco-pompe-skill/src/test/java/utils/ResponseAssertions.java
@@ -2,6 +2,7 @@ package utils;
 
 import static java.util.Collections.singletonList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.amazon.ask.model.Response;
 import com.amazon.ask.model.ui.AskForPermissionsConsentCard;
@@ -11,17 +12,21 @@ import com.amazon.ask.model.ui.SsmlOutputSpeech;
 public class ResponseAssertions {
 
 	public static void assertSpeech(String text, Response response) {
-		assertEquals("<speak>" + text + "</speak>", ((SsmlOutputSpeech) response.getOutputSpeech()).getSsml());
+		SsmlOutputSpeech ssmlOutputSpeech = (SsmlOutputSpeech) response.getOutputSpeech();
+		assertNotNull(ssmlOutputSpeech, "No output speech was found in the reponse");
+		assertEquals("<speak>" + text + "</speak>", ssmlOutputSpeech.getSsml());
 	}
 
 	public static void assertCard(String content, Response response) {
 		SimpleCard card = (SimpleCard) response.getCard();
+		assertNotNull(card, "No card was found in the reponse");
 		assertEquals("Éco Pompe", card.getTitle());
 		assertEquals(content, card.getContent());
 	}
 
 	public static void assertCardWithPermissions(Response response) {
 		AskForPermissionsConsentCard card = (AskForPermissionsConsentCard) response.getCard();
+		assertNotNull(card, "No card was found in the reponse");
 		assertEquals(singletonList("read::alexa:device:all:address"), card.getPermissions());
 	}
 	
